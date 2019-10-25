7f8d5c38ddcce441fb31cbd0b97766ec75627583
odun <biodun.agbolade@gmail.com>
2019-10-25 11:26:38 +0100

    Logincode

---
+++ admin/logincode.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+require_once '../mine/dbh.php';
+
+$email = $_POST['email'];
+$pwd = $_POST['pwd'];
+
+$sql = "SELECT * FROM admin WHERE email='$email'";
+$result = $dbh->query($sql);
+$row = $result->fetch_assoc();
+$hash_pwd = $row['pwd'];
+$hash = password_verify($pwd, $hash_pwd);  //This function decrypt the password
+
+if($hash == 0){
+    header("Location: ../index.php?error=empty");
+    exit();
+}else {
+    $sql = "SELECT * FROM admin WHERE email='$email' AND pwd='$hash_pwd'";
+        $result = $dbh->query($sql);
+
+    if(!$row = $result->fetch_assoc()){
+        echo "Your Username or Password is incorrect!";
+    }else{
+        $_SESSION['id'] = $row['id'];
+    }
+
+    header('Location: dashboard.php');
+}
